a31349d901ff520db24137d8542e3e1d0476dce4
Yaroslav-Programmer <51450308+Yaroslav-Programmer@users.noreply.github.com>
2019-06-30 14:11:25 +0300

    Add files via upload

--- New_Loop.c
+++ New_Loop.c
@@ -5,9 +5,9 @@
 /* печать таблицы температур по Фаренгейту и Цельсию */
 int main (void)
 {
-	int fahr;
-	for (fahr = UPPER; fahr <= LOWER; fahr -= STEP){
+	int fahrenheit;
+	for (fahrenheit = UPPER; fahrenheit >= LOWER; fahrenheit -= STEP) {
 	printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 	}
-	return 0;
+ 
 }
